d878df42c763d6cdcf6777e79dfd793af77c4361
Cara Steffen <cara@treadstonedesign.com>
2019-10-11 15:30:33 -0600

    fixed gramatical error

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const { NODE_ENV } = require("./config");
 
 const app = express();
 
-const morganOption = NODE_ENV === "production" ? "tiny" : "commom";
+const morganOption = NODE_ENV === "production" ? "tiny" : "common"; //or use dev in place of common
 
 app.use(morgan(morganOption));
 app.use(helmet());
